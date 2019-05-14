ef01db89e01c60bc397b71f5892b43f47800a8cd
argv-minus-one <argv-minus-one@users.noreply.github.com>
2019-05-14 00:07:08 -0700

    ErrorBuffer: Deduplicate passed-in Error objects.

--- src/util/errors.ts
+++ src/util/errors.ts
@@ -12,13 +12,37 @@ export class ErrorBuffer {
 	 * Adds the given error(s) (or other objects, which are converted to errors).
 	 */
 	add(...errors: unknown[]): void {
-		for (const error of errors) {
+		for (let error of errors) {
 			if (error instanceof MultiError)
 				this.add(...error.errors());
-			else
-				this.errors.push(
-					error instanceof Error ? error : new Error(String(error))
-				);
+			else {
+				if (!(error instanceof Error))
+					error = new Error(String(error));
+				else if (this.errors.indexOf(error) !== -1) {
+					/* Deduplicate errors.
+					 *
+					 * Consider this scenario:
+					 * 1. Promise A is started.
+					 * 2. Promise B is started. It awaits the result of A.
+					 * 3. Promise C is started. It also awaits the result of A.
+					 * 4. PromiseEach is called, to collect the results of promises B and C.
+					 * 5. Promise A rejects with error E.
+					 * 6. Promise B, previously waiting on A, rejects with E.
+					 * 7. Promise C, previously waiting on A, also rejects with E.
+					 * 8. PromiseEach collects the results of [B, C]. They are { B: rejection(E), C: rejection(E) }.
+					 * 9. PromiseEach finds that B rejected with E, so it adds E to its ErrorBuffer.
+					 * 10. PromiseEach finds that C rejected with E, so it adds E to its ErrorBuffer.
+					 * 11. PromiseEach rejects with [E, E].
+					 *
+					 * But, if ErrorBuffer deduplicates the errors it receives, then step 10 has no effect, because E is already in the ErrorBuffer. As a result, in step 11, PromiseEach rejects with E instead of [E, E].
+					 *
+					 * Note that this deduplication only applies to instances of Error. When other values are passed in, they are converted to a new instance of Error each time, so there is no chance to deduplicate them.
+					 */
+					continue;
+				}
+
+				this.errors.push(error as Error);
+			}
 		}
 	}
 
