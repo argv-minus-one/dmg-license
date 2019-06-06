5936c8a1b55c5ff268622555959cc43cc9c67297
argv-minus-one <argv-minus-one@users.noreply.github.com>
2019-06-05 19:04:15 -0700

    Convert the indentation of GetLanguageNames.java to tabs.

--- language-info-generator/GetLanguageNames.java
+++ language-info-generator/GetLanguageNames.java
@@ -5,32 +5,32 @@ import static java.lang.System.out;
 import static java.util.Locale.ENGLISH;
 
 public final class GetLanguageNames {
-  public static void main(String[] args) throws Exception {
-    if (args.length == 0) {
-      err.println("This program should be passed, on the command line, a list of locales.");
-      err.println("Example: java … GetLanguageNames en fr pt-br");
-      exit(1);
-    }
+	public static void main(String[] args) throws Exception {
+		if (args.length == 0) {
+			err.println("This program should be passed, on the command line, a list of locales.");
+			err.println("Example: java … GetLanguageNames en fr pt-br");
+			exit(1);
+		}
 
-    out.println("# Language tag\tEnglish display name\tLocalized display name");
+		out.println("# Language tag\tEnglish display name\tLocalized display name");
 
-    boolean hadErrors = false;
+		boolean hadErrors = false;
 
-    for (String arg : args) {
-      Locale locale = Locale.forLanguageTag(arg);
+		for (String arg : args) {
+			Locale locale = Locale.forLanguageTag(arg);
 
-      if (locale.equals(Locale.ROOT)) {
-        err.println("Invalid language tag: " + arg);
-        hadErrors = true;
-        continue;
-      }
+			if (locale.equals(Locale.ROOT)) {
+				err.println("Invalid language tag: " + arg);
+				hadErrors = true;
+				continue;
+			}
 
-      out.println(arg + '\t' + locale.getDisplayName(ENGLISH) + '\t' + locale.getDisplayName(locale));
-    }
+			out.println(arg + '\t' + locale.getDisplayName(ENGLISH) + '\t' + locale.getDisplayName(locale));
+		}
 
-    if (hadErrors) {
-      err.println("Warning: Some language tags were invalid. Output is incomplete.");
-      exit(1);
-    }
-  }
+		if (hadErrors) {
+			err.println("Warning: Some language tags were invalid. Output is incomplete.");
+			exit(1);
+		}
+	}
 }
